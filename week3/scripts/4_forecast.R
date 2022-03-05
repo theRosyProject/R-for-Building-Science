5e0cf3b2fe9087c7dbdc747b45b4a64433b556bf
Federico Dallo <36605177+theRosyProject@users.noreply.github.com>
2022-03-04 17:54:09 -0800

    fix: adding pdf and doc to ignore and update 4_forecast

--- week3/scripts/4_forecast.R
+++ week3/scripts/4_forecast.R
@@ -68,11 +68,30 @@ glimpse(meteo_01M) # alternative to str()
 
 ## -- convert into tsibble --##
 
-meteo_01M %>%
+meteo_01M <- meteo_01M %>%
   mutate(dttyDatetime = lubridate::ymd_hms(datetime),
-         Quarter = yearquarter(datetime)) %>%
-  glimpse()
+         Quarter = yearquarter(datetime))
 
+
+meteo_01M %>%
+  select(dttyDatetime, mean_T_C) %>%
+  #filter(mean_T_C > -20) %>%
+  ##FEDE## create daily average data:
+  mutate(YEAR = lubridate::year(dttyDatetime),
+         MONTH = lubridate::month(dttyDatetime),
+         DAY = lubridate::day(dttyDatetime)
+         ) %>%
+  group_by(YEAR, MONTH, DAY) %>%
+  summarise(DATE = lubridate::floor_date(x = mean(dttyDatetime, na.rm=TRUE), unit = "day"),
+         TEMPERATURE = mean(mean_T_C, na.rm = T)
+         ) %>%
+  ungroup() %>%
+  select(DATE, TEMPERATURE) %>%
+  #nrow()
+  ##FEDE## plot:
+  ggplot(data = ., aes(x = DATE, y = TEMPERATURE)) +
+  geom_line() + 
+  geom_point()
 
 
 
